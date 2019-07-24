292b9d8731319761f948bdf5b0912a0051e51a02
ethanol20924 <20924@bbc.qld.edu.au>
2019-07-24 23:02:28 +1000

    changed framesize in setCTAR to 8

--- Teensy/src/main.cpp
+++ Teensy/src/main.cpp
@@ -8,7 +8,7 @@ void setup() {
 	digitalWrite(13, HIGH);
 
 	spi.begin_SLAVE(SCK, MOSI, MISO, CS0);
-	spi.setCTAR_SLAVE(16, SPI_MODE0);
+	spi.setCTAR_SLAVE(8, SPI_MODE0);
 
 	// Enable the SPI0 Interrupt
   	NVIC_ENABLE_IRQ(IRQ_SPI0);
