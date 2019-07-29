fc02377b0b25c3902d8bb1360d1de60988949c4f
ethanol20924 <20924@bbc.qld.edu.au>
2019-07-29 21:10:22 +1000

    bruhhhh it compiles

--- Teensy/src/main.cpp
+++ Teensy/src/main.cpp
@@ -1,8 +1,10 @@
 #include <Arduino.h>
 #include "Utils.h"
+#include "Config.h"
 #include "LightSensorArray.h"
 #include "IMU.h"
 #include "Timer.h"
+#include <i2c_t3.h>
 
 // I would instantiate a timer class so we can blink the led but im a fucking idiot and used pin 13 for SPI cos im a fucking idiot
 
@@ -12,14 +14,21 @@ LightSensorArray ls;
 // Variables which i couldn't be bothered to find a good place for
 float batteryVoltage;
 
+// I2C Stuff
+uint8_t dataOut[1]; // TODO
+char dataIn[1];
+
+void requestEvent(void);
+void receiveEvent(size_t count);
+
 void setup() {
     // Put other setup stuff here
     Serial.begin(9600);
 
     // join bus on address 0x12 (in slave mode)
-    Wire.begin(0x12);
-    Wire.onRequest(requestEvent);
-    Wire.onReceive(receiveEvent);
+    ESP_WIRE.begin(0x12);
+    ESP_WIRE.onRequest(requestEvent);
+    ESP_WIRE.onReceive(receiveEvent);
 
     // Init IMU stuff
     imu.init();
@@ -48,9 +57,11 @@ void loop() {
 }
 
 void requestEvent() {
-    // TODO
+    // TODO - Not the actual code lol
+    // ESP_WIRE.write(dataOut,DATA_SIZE);
 }
 
-void receiveEvent(int bytes) {
-    // TODO
+void receiveEvent(size_t count) {
+    // TODO - Not the actual code lol
+    // ESP_WIRE.read(dataIn,count);  // copy Rx data to databuf
 }
