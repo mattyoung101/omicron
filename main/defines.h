a74cf3af080d9ea134bc8d6398f28a4759340124
ethanol20924 <20924@bbc.qld.edu.au>
2019-09-30 16:24:59 +1000

    some orbit testing

--- main/defines.h
+++ main/defines.h
@@ -19,8 +19,8 @@
 #define BT_CONF_RES_DYNAMIC 1 // uses ball data to resolve conflicts
 #define BT_PACKET_TIMEOUT 1500 // ms, if we haven't received a packet in this long, other robot is off for damage
 #define BT_SWITCH_COOLDOWN 2500 // ms, wait this many ms after a switch before another switch is allowed
-// #define BLUETOOTH_ENABLED // whether or not Bluetooth is enabled
-// #define BT_SWITCHING_ENABLED // if Bluetooth role switching is enabled or not (off for damage detection aways runs)
+#define BLUETOOTH_ENABLED // whether or not Bluetooth is enabled
+#define BT_SWITCHING_ENABLED // if Bluetooth role switching is enabled or not (off for damage detection aways runs)
 #define BT_CONF_RES_MODE BT_CONF_RES_DYNAMIC // the conflict resolution mode
 #define DEFENCE false // whether to start out in defence (I think? unclear)
 #define BT_MAX_ERRORS 4 // max errors before dropping connection
@@ -176,7 +176,7 @@ extern uint16_t  IN_FRONT_MAX_ANGLE;
 #define IDLE_OFFSET 0
 #define DRIBBLE_TIMEOUT 100 // ms, if robot sees ball in this position for this time it will switch to dribble state
 #define DRIBBLE_SPEED 80 // speed at which robot dribbles the ball, out of 100
-#define ACCEL_PROG 0.1 // update the acceleration interpolation by this amount per tick, 1 tick is about 10ms, so 0.01 will accelerate completely in 1 second
+#define ACCEL_PROG 1 // update the acceleration interpolation by this amount per tick, 1 tick is about 10ms, so 0.01 will accelerate completely in 1 second
 #define GOAL_MIN_ANGLE 30
 #define GOAL_MAX_ANGLE 330
 #define GOAL_SHOOT_DIST 40 // if we are within this distance, shoot
