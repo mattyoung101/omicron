6ac7568bd18483f2213a9b3e1a7b4a63757c9f63
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2019-12-19 11:55:32 +1000

    Mouse Sensor Library Implemented

--- atmega/src/main.cpp
+++ atmega/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
+#include <mouse.h>
+
+PMW3360 mouse;
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(9600);
+    Serial.println("ready to pop");
+    Serial.println(mouse.begin(10));
+    Serial.println("aight we done");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    PMW3360_DATA data = mouse.readBurst();
+    Serial.print(data.dx);
+    Serial.print(", ");
+    Serial.println(data.dy);
 }
