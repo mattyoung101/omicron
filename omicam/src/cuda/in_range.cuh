5063c53502b4e9cf1b5d7882bddf30ba3945cc5c
Matt Young <matt.young.1@outlook.com>
2020-01-02 19:33:09 +1000

    Implemented GPU acceleration but it's very slow :(

---
+++ omicam/src/cuda/in_range.cuh
@@ -0,0 +1,21 @@
+#pragma once
+#ifndef SKIP_INCLUDES
+#include <vector>
+#include <memory>
+#include <iosfwd>
+#include <iostream>
+#endif
+
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/core/core.hpp"
+#include "opencv2/core/cuda.hpp"
+#include "opencv2/videoio.hpp"
+#include "opencv2/cudafilters.hpp"
+#include "opencv2/cudaimgproc.hpp"
+#include "opencv2/cudaarithm.hpp"
+
+// Source for this: https://github.com/opencv/opencv/issues/6295#issuecomment-321727869
+
+void inRange_gpu(cv::cuda::GpuMat &src, cv::Scalar &lowerb, cv::Scalar &upperb,
+                 cv::cuda::GpuMat &dst);
