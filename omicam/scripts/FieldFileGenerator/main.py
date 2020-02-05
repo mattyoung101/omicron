b35678f13c671532f96cd311e40d898a0162b656
Matt Young <matt.young.1@outlook.com>
2020-02-05 12:22:55 +1000

    Modified field file generator for new objective function

--- omicam/scripts/FieldFileGenerator/main.py
+++ omicam/scripts/FieldFileGenerator/main.py
@@ -1,19 +1,18 @@
-from lineFunctions import linearFunction, circularFunction
+from lineFunctions import linearFunction, circularFunction, euclideanDist
 from objectiveFunction import virtualRobot, linePoint
 from math import sqrt, pow, radians
 import itertools
 from PIL import Image
 import FieldFile_pb2 as pb
 
-def constrain(val, smol, big):
-    return min(max(val, smol), big)
-
 # Rules and field specifications:
 # https://junior.robocup.org/wp-content/uploads/2020Rules/2020_Soccer_Rules_draft01.pdf
 
 fieldLength = 243 # cm
 fieldWidth = 182
-output = [[None for x in range(fieldWidth)] for y in range(fieldLength)] # Note: notation is output[y][x]
+lineWidth = 2 # width of field lines in cm
+
+output = [[None for x in range(fieldLength)] for y in range(fieldWidth)] # Note: notation is output[y][x]
 whiteLines = [
     linearFunction(-66, 96.5, -66, -96.5),
     linearFunction(-66, 96.5, 66, 96.5),
@@ -31,53 +30,39 @@ whiteLines = [
     circularFunction(radians(270), radians(360), 15, 20, 86.5)
 ]
 
-robot = virtualRobot(500, 1000, 0)
+robot = virtualRobot(0, 0, 0)
 
 # --- GENERATES FIELD FILE ARRAY --- #
 
-for rownum, row in enumerate(output):
-    for colnum, cell in enumerate(row):
+for colnum, row in enumerate(output):
+    for rownum, cell in enumerate(row):
         smallestDist = sqrt(pow(fieldLength, 2) + pow(fieldWidth, 2))
         for line in whiteLines:
             smallestDist = min(line.smallestDist(colnum - fieldWidth / 2, rownum - fieldLength / 2), smallestDist)
-        output[rownum][colnum] = smallestDist
+        output[colnum][rownum] = smallestDist
 
-# --- FAKE OBJECTIVE FUNCTION --- #
+maxDist = max([sublist[-1] for sublist in output])
 
-objectiveMap = [[None for x in range(fieldWidth)] for y in range(fieldLength)] # Note: notation is output[y][x]
+for colnum, row in enumerate(output):
+    for rownum, cell in enumerate(row):
+        # check if it's on the line before drawing
+        if cell <= lineWidth:
+            output[colnum][rownum] = 255
+        else:
+            output[colnum][rownum] = 0
 
-for X in range(0, fieldWidth):
-    x = X - fieldWidth / 2
-    for Y in range(0, fieldLength):
-        y = Y - fieldLength / 2
-        runningTotal = 0
-        for point in robot.points:
-            runningTotal += output[constrain(int(round(point.y + fieldLength / 2 + y)), 0, fieldLength - 1)][constrain(int(round(point.x + fieldWidth / 2 + x)), 0, fieldWidth - 1)]
-        objectiveMap[Y][X] = runningTotal
+output = list(itertools.chain(*output))
 
-# print(objectiveMap)
-
-maxDist = max([sublist[-1] for sublist in objectiveMap])
-
-for rownum, row in enumerate(output):
-    for colnum, cell in enumerate(row):
-        objectiveMap[rownum][colnum] = int(cell / maxDist * 255) * -1 + 255
-
-output = list(itertools.chain(*objectiveMap))
-
-for row in output:
-    print(row)
-
-img = Image.new('L', (fieldWidth, fieldLength))
+img = Image.new('L', (fieldLength, fieldWidth))
 img.putdata(output)
-# img.save("Standard Field.bmp")
+img.save("field.bmp")
 img.show()
 
 # message = pb.FieldFile()
 # message.unitDistance = 1
 # message.cellCount = int(fieldLength * fieldWidth)
-# message.width = int(fieldWidth)
-# message.length = int(fieldLength)
+# message.fieldWidth = int(fieldWidth)
+# message.fieldHeight = int(fieldLength)
 # message.data.extend(output)
 #
 # with open("../../fields/Ints_StandardField.ff", "wb") as file:
