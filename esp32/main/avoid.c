2b8dc76e8e2526fea39af18857de1ca068c1a574
A-Dragons-Tail <37792607+A-Dragons-Tail@users.noreply.github.com>
2020-02-19 09:02:59 +1000

    orbit compiles and added to cmakelists

--- esp32/main/avoid.c
+++ esp32/main/avoid.c
@@ -1,54 +1,53 @@
-#include "orbit.h"
-#include "utils.h"
+#include "avoid.h"
 
-bool orbit::isInEllipse(Vector robot, float a, float b)
+static bool isInEllipse(vect_2d_t robot, float a, float b)
 {
-    Vector temp1 = Vector(robot.i, robot.j, false);
-    float angle = atan2(robot.j, robot.i);
-    Vector temp2 = Vector(a * cos(angle), b * sin(angle), false);
+    vect_2d_t temp1 = vect_2d(robot.x, robot.y, false);
+    float angle = atan2(robot.y, robot.x);
+    vect_2d_t temp2 = vect_2d(a * cos(angle), b * sin(angle), false);
     return temp2.mag > temp1.mag;
 }
-Vector orbit::ellipsePoint(Vector robot, float a, float b)
+static vect_2d_t ellipsePoint(vect_2d_t robot, float a, float b)
 {
-    //fuckin bitch ass i swear to god i will literally shoot you
-    float angle = atan2(robot.j, robot.i);
-    return Vector(a * cos(angle), b * sin(angle), false);
+    //replace with better nearest point function when angus is done
+    float angle = atan2(robot.y, robot.x);
+    return vect_2d(a * cos(angle), b * sin(angle), false);
 }
 
-inline float orbit::mCalc(Vector robot, float a, float b)
+static inline float mCalc(vect_2d_t robot, float a, float b)
 {
-    return (-powf(b, 2) * robot.i) / (powf(a, 2) * robot.j);
+    return (-powf(b, 2) * robot.x) / (powf(a, 2) * robot.y);
 }
 
-inline float orbit::cCalc(Vector robot, float a, float b)
+static inline float cCalc(vect_2d_t robot, float a, float b)
 {
-    return (powf(a, 2) * powf(b, 2)) / (powf(a, 2) * robot.j);
+    return (powf(a, 2) * powf(b, 2)) / (powf(a, 2) * robot.y);
 }
 
-inline float orbit::x(Vector robot, float a, float b, int p)
+static inline float x(vect_2d_t robot, float a, float b, int p)
 {
     float m = mCalc(robot, a, b);
     float c = cCalc(robot, a, b);
-    return ((-powf(a, 2) * m * c) + p * sign(robot.j) * a * b * sqrtf((pow(a, 2) * pow(m, 2)) + pow(b, 2) - pow(c, 2))) / (pow(b, 2) + (pow(a, 2) * pow(m, 2)));
+    return ((-powf(a, 2) * m * c) + p * sign(robot.y) * a * b * sqrtf((pow(a, 2) * pow(m, 2)) + pow(b, 2) - pow(c, 2))) / (pow(b, 2) + (pow(a, 2) * pow(m, 2)));
 }
 
-inline float orbit::y(Vector robot, float a, float b, int p)
+static inline float y(vect_2d_t robot, float a, float b, int p)
 {
     float m = mCalc(robot, a, b);
     float c = cCalc(robot, a, b);
-    return ((powf(b, 2) * c) + p * sign(robot.j) * a * b * m * sqrtf((pow(a, 2) * pow(m, 2)) + pow(b, 2) - pow(c, 2))) / (pow(b, 2) + (pow(a, 2) * pow(m, 2)));
+    return ((powf(b, 2) * c) + p * sign(robot.y) * a * b * m * sqrtf((pow(a, 2) * pow(m, 2)) + pow(b, 2) - pow(c, 2))) / (pow(b, 2) + (pow(a, 2) * pow(m, 2)));
 }
 
-Vector orbit::calcAvoid(float a, float b, Vector obj, Vector robot)
+static vect_2d_t calcAvoid(float a, float b, vect_2d_t obj, vect_2d_t robot)
 {
-    Vector pointOne = Vector(x(robot, a, b, -1), y(robot, a, b, -1), false);
-    Vector pointTwo = Vector(x(robot, a, b, 1), y(robot, a, b, 1), false);
+    vect_2d_t pointOne = vect_2d(x(robot, a, b, -1), y(robot, a, b, -1), false);
+    vect_2d_t pointTwo = vect_2d(x(robot, a, b, 1), y(robot, a, b, 1), false);
 
     if (isInEllipse(robot, a, b))
     {
         return ellipsePoint(robot, a, b);
     }
-    else if (isAngleBetween(obj.arg, pointTwo.arg, pointOne.arg))
+    else if (is_angle_between(obj.arg, pointTwo.arg, pointOne.arg))
     {
         if (smallestAngleBetween(obj.arg, pointOne.arg) < smallestAngleBetween(pointTwo.arg, obj.arg))
         {
@@ -65,8 +64,7 @@ Vector orbit::calcAvoid(float a, float b, Vector obj, Vector robot)
     }
 }
 
-Vector orbit::avoidMethod(Vector avoid, float a, float b, Vector obj, Vector robot)
-{
-    Vector movement = calcAvoid(a, b, obj, Vector(robot.i - avoid.i, robot.j - avoid.j, false));
-    return Vector(movement.i + avoid.i, movement.j + avoid.j, false);
+vect_2d_t avoidMethod(vect_2d_t avoid, float a, float b, vect_2d_t obj, vect_2d_t robot) {
+    vect_2d_t movement = calcAvoid(a, b, obj, vect_2d(robot.x - avoid.x, robot.y - avoid.y, false));
+    return vect_2d(movement.x + avoid.x, movement.y + avoid.y, false);
 }
---
+++ esp32/main/avoid.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "utils.h"
+#include "Vector.h"
+#include "stdbool.h"
+#include "math.h"
+
+static bool isInEllipse(vect_2d_t robot, float a, float b);
+static vect_2d_t ellipsePoint(vect_2d_t robot, float a, float b);
+static inline float mCalc(vect_2d_t robot, float a, float b);
+static inline float cCalc(vect_2d_t robot, float a, float b);
+static inline float x(vect_2d_t robot, float a, float b, int p);
+static inline float y(vect_2d_t robot, float a, float b, int p);
+static vect_2d_t calcAvoid(float a, float b, vect_2d_t obj, vect_2d_t robot);
+vect_2d_t avoidMethod(vect_2d_t avoid, float a, float b, vect_2d_t obj, vect_2d_t robot);
