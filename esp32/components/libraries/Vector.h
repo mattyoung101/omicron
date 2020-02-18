026c651fb28ccfe9d651b6921ee88092e618f4c1
ethanol20924 <20924@bbc.qld.edu.au>
2020-02-19 09:52:50 +1000

    warnings are gone

--- esp32/components/libraries/Vector.h
+++ esp32/components/libraries/Vector.h
@@ -7,10 +7,10 @@ typedef struct vect_2d_t
     float x, y, mag, arg;
 } vect_2d_t;
 
-static float calcMag(float x, float y);
-static float calcArg(float x, float y);
-static float calcX(float mag, float arg);
-static float calcY(float mag, float arg);
+// float calcMag(float x, float y);
+// float calcArg(float x, float y);
+// float calcX(float mag, float arg);
+// float calcY(float mag, float arg);
 vect_2d_t vect_2d(float xOrMag, float yOrArg, bool isPolar);
 vect_2d_t add_vect_2d(vect_2d_t one, vect_2d_t two);
 vect_2d_t subtract_vect_2d(vect_2d_t one, vect_2d_t two);
