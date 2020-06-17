9ec76ccb5a43f7bb9b89cca21aa18f4ab745225f
Matt Young <matt.young.1@outlook.com>
2020-06-18 00:17:14 +1000

    After much pain, the Kotlin camera dewarper is almost done. It's not perfect but this is causing me so much grief that I'm going to give up after nearest neighbour interpolation works.

--- omicam/scripts/dewarp.py
+++ omicam/scripts/dewarp.py
@@ -6,6 +6,11 @@ from scipy.spatial import distance
 from scipy.interpolate import NearestNDInterpolator
 import sys
 
+##############################################################
+## ATTENTION: THIS SCRIPT SUCKS AND IS DEPRECATED           ##
+## IT HAS BEEN REPLACED BY CameraDewarper.kt in Omicontrol. ##
+##############################################################
+
 SCALAR = 4
 MIRROR_RADIUS = 384
 
@@ -16,12 +21,16 @@ def linearise(x):
 def lerp(fromValue, toValue, progress):
     return fromValue + (toValue - fromValue) * progress
 
+def constrain(val, min_val, max_val):
+    return min(max_val, max(min_val, val))
+
 if __name__ == "__main__":
     in_img = Image.open("frame2.jpg")
     width, height = in_img.size
-    out_img = Image.new("RGB", (MIRROR_RADIUS * 2 + 32, MIRROR_RADIUS * 2 + 32))
-    out_width, out_height = out_img.size
-    out_mag_range_px = distance.euclidean((0, 0), (out_width, out_height))
+    out_width = MIRROR_RADIUS * 2 + 32
+    out_height = out_width
+
+    out_mag_range_px = distance.euclidean((0, 0), (out_width / 2, out_height / 2))
     print(f"Output size: {out_width}x{out_height}, mag range pixels {out_mag_range_px}")
 
     points = []
@@ -53,7 +62,11 @@ if __name__ == "__main__":
                 smallest_pos = [x, y]
 
     print(f"Largest mag: {largest_mag} at coordinate {largest_pos[0]},{largest_pos[1]}")
-    print(f"Smallest mag: {smallest_mag} at coordinate {smallest_pos[0]},{smallest_pos[1]}")  
+    print(f"Smallest mag: {smallest_mag} at coordinate {smallest_pos[0]},{smallest_pos[1]}")
+
+    pixels = {}
+    largest_x = -999
+    largest_y = -999
 
     print("Dewarping...")
     for y in range(height):
@@ -75,18 +88,27 @@ if __name__ == "__main__":
             # convert back to cartesian to be drawn
             cart = cmath.rect(r_scaled, phi) 
             # add back half the image width to get back to the centre
-            cart = complex(cart.real + (out_width / 2), cart.imag + (out_height / 2)) 
+            cart = complex(cart.real + (width / 2), cart.imag + (height / 2)) 
             
             points.append([cart.real, cart.imag])
             values.append([col_r, col_g, col_b])
+        
+            c_x = floor(cart.real) #constrain(cart.real, 0, out_width - 1)
+            c_y = floor(cart.imag) #constrain(cart.imag, 0, out_height - 1)
+            pixels[f"{c_x} {c_y}"] = [col_r, col_g, col_b]
 
-            try:
-                out_img.putpixel((floor(cart.real), floor(cart.imag)), (col_r, col_g, col_b))
-            except IndexError:
-                # TODO WHY THE FUCK DOES THIS SHIT KEEP GOING NEGATIVE, WRITE IT TO ANOTHER FILE?????
-                #print(f"Fucked up, trying to write to: {cart.real}, {cart.imag}: r is {r}, scaled r: {r_scaled}, angle: {phi}")
-                #sys.exit(0)
-                continue
+            if c_x > largest_x:
+                largest_x = c_x
+            elif c_y > largest_y:
+                largest_y = c_y
+
+    print(f"LARGEST X Y: {largest_x}, {largest_y}")
+
+    print("Generating image...")
+    out_img = Image.new("RGB", (largest_x + 8, largest_y + 8))
+    for key, value in pixels.items():
+        x_coord, y_coord = key.split()
+        out_img.putpixel((int(x_coord), int(y_coord)), (value[0], value[1], value[2]))
 
     visited = 0
     last_progress = 0
@@ -109,5 +131,5 @@ if __name__ == "__main__":
     #             print(f"{complete}% complete")
     #             last_progress = complete
 
-    out_img.save("out2.png", "PNG")
+    out_img.save("out.png", "PNG")
     out_img.show()
--- omicontrol/src/main/kotlin/com/omicron/omicontrol/tools/CameraDewarper.kt
+++ omicontrol/src/main/kotlin/com/omicron/omicontrol/tools/CameraDewarper.kt
@@ -1,20 +1,146 @@
 package com.omicron.omicontrol.tools
 
-import net.objecthunter.exp4j.ExpressionBuilder
+import javafx.geometry.Point2D
+import java.awt.Color
+import java.awt.Rectangle
+import java.awt.image.BufferedImage
+import java.nio.file.Paths
+import javax.imageio.ImageIO
+import kotlin.math.*
+
+/** A location in the image */
+data class Location(val x: Int, val y: Int)
 
 /**
  * Utility program which dewarps a frame from the camera based on the mirror model
+ * @author Matt Young
  */
 object CameraDewarper {
-    private const val MIRROR_MODEL = "2.5874667517468426 * exp(0.012010445463214335 * x)"
-    private const val OUTPUT_WIDTH = 1280
-    private const val OUTPUT_HEIGHT = 720
+    private const val MIRROR_RADIUS = 384
+    /** size to attempt to constrain dewarp output into a square of */
+    private const val OUT_SIZE: Double = MIRROR_RADIUS * 1.0
+    private val OUT_DIAGONAL = sqrt(OUT_SIZE.pow(2) + OUT_SIZE.pow(2)) / 2.0
+
+    /**
+     * Mirror dewarp model, directly from the calculated regression in the Calibration View in Omicontrol.
+     */
+    private fun dewarp(x: Double): Double {
+        return 2.5874667517468426 * exp(0.012010445463214335 * x)
+    }
 
     @JvmStatic
     fun main(args: Array<String>){
-        val model = ExpressionBuilder(MIRROR_MODEL).variables("x").build()
+        val image = ImageIO.read(Paths.get("test_data/frame2.jpg").toFile())
+        val centre = Point2D(image.width / 2.0, image.height / 2.0)
+        val pixels = hashMapOf<Location, Int>()
 
-        // the largest and smallest domains of the mirror dewarp function should be from the centre to the corners
-        // of the frame
+        println("Out diagonal: $OUT_DIAGONAL")
+
+        println("Finding magnitude range...")
+        var minMag = 9999.0
+        var maxMag = -9999.0
+        for (y in 0 until image.height){
+            for (x in 0 until image.width){
+                // Make a vector from the centre
+                val vec = Point2D(x.toDouble(), y.toDouble()).subtract(centre)
+
+                // If the magnitude of the vector from the centre is outside the mirror, skip
+                if (vec.magnitude() > MIRROR_RADIUS) continue
+
+                // Calculate dewarped magnitude
+                val mag = dewarp(vec.magnitude())
+                if (mag > maxMag){
+                    maxMag = mag
+                } else if (mag < minMag){
+                    minMag = mag
+                }
+            }
+        }
+        println("Min dewarped magnitude: $minMag, Max dewarped magnitude: $maxMag")
+
+        println("Dewarping...")
+        for (y in 0 until image.height){
+            for (x in 0 until image.width){
+                // Make a vector from the centre
+                val vec = Point2D(x.toDouble(), y.toDouble()).subtract(centre)
+
+                // If the magnitude of the vector from the centre is outside the mirror, skip
+                if (vec.magnitude() > MIRROR_RADIUS) continue
+
+                // Convert to polar and scale magnitude
+                val polarAngle = atan2(vec.y, vec.x)
+                // note: uncomment the dewarp() part here to make a cool warp grid, somehow! no idea how that works
+                val polarMag = dewarp(vec.magnitude())
+                val scaledMag = ((polarMag - minMag) / (maxMag - minMag)) * OUT_DIAGONAL
+
+                // Convert back to cartesian
+                val dewarped = Point2D(scaledMag * cos(polarAngle), scaledMag * sin(polarAngle))
+                    .add(centre.multiply(0.5))
+
+                // Add dewarped location with current colour to pixels grid
+                pixels[Location(dewarped.x.roundToInt(), dewarped.y.roundToInt())] = image.getRGB(x, y)
+            }
+        }
+
+//        val out = BufferedImage(ceil(OUT_SIZE).toInt() + 64, ceil(OUT_SIZE).toInt() + 64, image.type)
+//        println("Image size: ${out.width}x${out.height}")
+//        for ((key, value) in pixels.entries){
+//            val x = if (key.x == out.width) key.x - 1 else key.x
+//            val y = if (key.y == out.height) key.y - 1 else key.y
+//            try {
+//                out.setRGB(x, y, value)
+//            } catch (e: Exception){
+//                println("FUCK YOU, COULDN'T SET $x,$y")
+//            }
+//        }
+
+        // Find largest x value
+        val largestX = pixels.keys.maxBy { it.x }!!.x
+        val largestY = pixels.keys.maxBy { it.y }!!.y
+        val smallestX = pixels.keys.minBy { it.x }!!.x
+        val smallestY = pixels.keys.minBy { it.y }!!.y
+        println("Largest XY: $largestX,$largestY Smallest XY: $smallestX,$smallestY")
+
+        // Write out the image
+        val out = BufferedImage(largestX + 64, largestY + 64, image.type)
+        var minX = 999
+        var maxX = -999
+        var minY = 999
+        var maxY = -99
+        for ((key, value) in pixels.entries){
+            val x = if (key.x == largestX) key.x - 1 else key.x
+            val y = if (key.y == largestY) key.y - 1 else if (key.y < 0) 32 else key.y + 32
+
+            try {
+                out.setRGB(x, y, value)
+
+                // we do this to find the bounding box of the mirror for cropping
+                if (x > maxX){
+                    maxX = x
+                } else if (x < minX){
+                    minX = x
+                } else if (y > maxY){
+                    maxY = y
+                } else if (y < minY){
+                    minY = y
+                }
+            } catch (e: Exception){
+                println("FUCK YOU, COULDN'T SET $x,$y")
+            }
+        }
+        print("Min/max written X: $minX, $maxX")
+
+        out.setRGB(minX, out.height / 2, Color.RED.rgb)
+        out.setRGB(maxX, out.height / 2, Color.RED.rgb)
+        out.setRGB(out.width / 2, minY, Color.RED.rgb)
+        out.setRGB(out.width / 2, maxY, Color.RED.rgb)
+
+        // TODO nearest neighbour interp missed pixels in image - only crop if inside bounding rectangle
+        val bounds = Rectangle(minX, minY, maxX - minX, maxY - minY)
+
+        // crop output (don't know why we have to do this, such is life)
+        val cropped = out.getSubimage(minX, minY, maxX - minX, maxY - minY)
+
+        ImageIO.write(cropped, "png", Paths.get("test_data/out.png").toFile())
     }
 }
